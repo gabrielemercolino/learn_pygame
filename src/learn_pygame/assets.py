0fa728cbf5fd7ad56863ae1b1065fe7ebb03590d
Gabriele <gmercolino2003@gmail.com>
2024-10-14 12:41:54 +0200

    feat: added player animations

--- src/learn_pygame/assets.py
+++ src/learn_pygame/assets.py
@@ -1,4 +1,5 @@
 import pygame
+from learn_pygame.animations import Animation
 from learn_pygame.utils import load_image, load_images
 
 BLACK = (0, 0, 0)
@@ -7,6 +8,8 @@ class Assets:
     tiles: dict[str, list[pygame.Surface]]
     player: pygame.Surface
     background: pygame.Surface
+    clouds: list[pygame.Surface]
+    animations: dict[str, dict[str, Animation]]
 
     @staticmethod
     def load():
@@ -18,5 +21,14 @@ class Assets:
         )
         Assets.player = load_image("entities/player.png", color_key=BLACK)
         Assets.background = load_image("background.png")
-        
+        Assets.clouds = load_images("clouds", color_key=BLACK)
     
+        Assets.animations = dict(
+            player = dict(
+                idle = Animation(images=load_images("entities/player/idle", color_key=BLACK), duration=6),
+                run = Animation(images=load_images("entities/player/run", color_key=BLACK), duration=4),
+                jump = Animation(images=load_images("entities/player/jump", color_key=BLACK)),
+                slide = Animation(images=load_images("entities/player/slide", color_key=BLACK)),
+                wall_slide = Animation(images=load_images("entities/player/wall_slide", color_key=BLACK)),
+            )
+        )
