ddfba8527d5465d1efe8b8ca989c4cc29c420672
Gabriele <gmercolino2003@gmail.com>
2024-10-14 09:34:32 +0200

    chore: removed update thread and added clouds

--- src/learn_pygame/main.py
+++ src/learn_pygame/main.py
@@ -1,6 +1,7 @@
-import sys, threading
+import sys
 import pygame
 from learn_pygame.assets import Assets
+from learn_pygame.clouds import Clouds
 from learn_pygame.entities import PhysicsEntity
 from learn_pygame.tilemap import Tilemap
 
@@ -22,15 +23,13 @@ class Game:
 
         self.player = PhysicsEntity('player', (50, 50), (8, 15))
         self.tilemap = Tilemap()
+        self.clouds = Clouds(cloud_count=10)
 
         self.camera_offset = pygame.Vector2(0, 0)
 
         self.running = True
 
     def run(self) -> None:
-        logic_thread = threading.Thread(target=self.update)
-        logic_thread.start()
-
         while self.running:
             # clear screen
             #self.display.fill((14, 14, 14))
@@ -39,12 +38,16 @@ class Game:
             # check events like keyboard input
             self.check_events(events = pygame.event.get())
 
-            # rendering
+            self.update()
+
             self.camera_offset.x += int((self.player.rect.centerx - self.display.get_width() / 2 - self.camera_offset.x) / 15)
             self.camera_offset.y += int((self.player.rect.centery - self.display.get_height() / 2 - self.camera_offset.y) / 15)
+
+            # rendering
+            self.clouds.render(destination=self.display, offset=self.camera_offset)
             self.tilemap.render(destination=self.display, offset=self.camera_offset)
             self.player.render(destination=self.display, offset=self.camera_offset)
-            
+
             # now show the frame
             self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0,0))
             pygame.display.update()
@@ -52,15 +55,11 @@ class Game:
             # wait until enough time has passed so 60fps can be archieved
             self.clock.tick(60)
 
-        self.running = False # it theory is not needed but whatever
-        logic_thread.join()
         sys.exit()
 
     def update(self) -> None:
-        while self.running:
-            self.player.update(self.tilemap, pygame.Vector2(self.input["right"]-self.input["left"], 0))
-
-            pygame.time.Clock().tick(60)
+        self.player.update(self.tilemap, pygame.Vector2(self.input["right"]-self.input["left"], 0))
+        self.clouds.update()
 
     def check_events(self, events: list[pygame.Event]):
         for event in events:
