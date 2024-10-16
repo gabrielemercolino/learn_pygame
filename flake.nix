3e2446a39325b729c56ca7e195afc5450592266f
Gabriele <gmercolino2003@gmail.com>
2024-10-16 08:56:27 +0200

    feat: added both editor and game apps

--- flake.nix
+++ flake.nix
@@ -6,17 +6,25 @@
       let
         pkgs = import nixpkgs { inherit system; };
         python = pkgs.python312;
-        project = pyproject-nix.lib.project.loadPyproject {
-          projectRoot = ./.;
-        };
-        attrs = project.renderers.buildPythonPackage { inherit python; };
+        
+        game = pyproject-nix.lib.project.loadPyproject { projectRoot = ./game; };
+        game-attrs = game.renderers.buildPythonPackage { inherit python; };
+        
+        editor = pyproject-nix.lib.project.loadPyproject { projectRoot = ./editor; };
+        editor-attrs = editor.renderers.buildPythonPackage { inherit python; };
       in
       {
-        packages.learn_pygame = python.pkgs.buildPythonPackage attrs; 
+        packages.learn_pygame_game = python.pkgs.buildPythonPackage game-attrs; 
+        packages.learn_pygame_editor = python.pkgs.buildPythonPackage editor-attrs;
 
         apps.default = {
           type = "app";
-          program = "${self.packages.${system}.learn_pygame}/bin/learn_pygame";
+          program = "${self.packages.${system}.learn_pygame_game}/bin/game";
+        };
+
+        apps.editor = {
+          type = "app";
+          program = "${self.packages.${system}.learn_pygame_editor}/bin/editor";
         };
 
         devShells.default = pkgs.mkShell {
